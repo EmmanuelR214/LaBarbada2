d8ab3a93725d2ab8ec52b98d84f6108a6e7dc47d
aldo <aldohernandezcelestino@gmail.com>
2024-03-30 14:56:27 -0600

    menu sider Update

--- src/views/public/Menu.jsx
+++ src/views/public/Menu.jsx
@@ -1,43 +1,12 @@
-import { useEffect,useState } from "react";
-import { useNavigate } from "react-router-dom";
-//import {useAuth} from "../../../routes/context/AuthContext"
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { Icon } from "@iconify/react";
 import { InputSearch } from "../../components/Inputs";
-import { motion } from "framer-motion";
 import { MenuButton } from "../../components/Buttons";
 
 const Menu = () => {
-  //  const { getMenu, listMenu } = useAuth();
+  const [sidebarVisible, setSidebarVisible] = useState(true);
 
-  const styleLink = "inline-block hover:bg-zinc-800 hover:rounded-lg p-2";
-  //Para abir y cerrar el menu siderbar
-  const [sidebarOpen, setSidebarOpen] = useState(true); // Inicia abierto en pantalla completa
-
-  useEffect(() => {
-    // Verifica el tamaño de la ventana para cambiar el estado del sidebar en responsivo
-    const handleWindowResize = () => {
-      if (window.innerWidth <= 640) { // Si es una pantalla pequeña
-        setSidebarOpen(false); // Cierra el sidebar en modo responsivo
-      } else {
-        setSidebarOpen(true); // Abre el sidebar en modo de escritorio
-      }
-    };
-
-    // Ejecuta la función de manejo del tamaño de la ventana cuando se carga y se redimensiona la ventana
-    window.addEventListener("resize", handleWindowResize);
-    handleWindowResize(); // Se ejecuta una vez para establecer el estado inicial
-
-    // Limpia el event listener al desmontar el componente
-    return () => window.removeEventListener("resize", handleWindowResize);
-  }, []);
-
-  const toggleSidebar = () => {
-    // Cambia el estado del sidebar solo en modo responsivo
-    if (window.innerWidth <= 640) {
-      setSidebarOpen(!sidebarOpen);
-    }
-  };
-  
   const menuItems = [
     { icon: "ph:shrimp", text: "Camarones" },
     { icon: "tabler:fish", text: "Pescados" },
@@ -57,77 +26,138 @@ const Menu = () => {
     { icon: "pepicons-pop:label-circle", text: "Promociones" },
     { icon: "lucide:cup-soda", text: "Bebidas" },
   ];
-  
-  
+
+  const styleLink =
+    "inline-block hover:bg-zinc-800 hover:rounded-lg p-2 text-white";
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth >= 1024) {
+        setSidebarVisible(true);
+      } else {
+        setSidebarVisible(false);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (window.innerWidth < 1024) {
+        const sidebar = document.getElementById("sidebar");
+        if (sidebar && !sidebar.contains(event.target)) {
+          setSidebarVisible(false);
+        }
+      }
+    }
+
+    if (sidebarVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sidebarVisible]);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
   return (
-    <div flex>
-      {/**BOTON QUE APARECE CUANDO SE DESPLIEGA */}
-      <button
-
-        type="button"
-        class="inline-flex items-center p-2 mt-2 ms-3 text-sm  rounded-lg sm:hidden dark:hover:bg-gray-700 bg-red-600"
-        onClick={toggleSidebar}
-      >
-        <span class="sr-only bg-amber-300">Open sidebar</span>
-        <Icon icon="ci:hamburger-md" />
-      </button>
-
+    <div className="min-h-screen flex relative">
       <motion.aside
-        id="default-sidebar"
-        className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
-          sidebarOpen ? "-translate-x-0" : "-translate-x-full"
+        id="sidebar"
+        className={`w-60 bg-red-600 p-5 fixed z-10 h-full lg:relative ${
+          sidebarVisible
+            ? "lg:static lg:z-auto lg:w-auto overflow-y-auto max-h-screen"
+            : "hidden"
         }`}
-        aria-label="Sidebar"
+        initial={false}
+        animate={{
+          x: sidebarVisible ? 0 : -240,
+          transition: { duration: 0.3, type: "tween" },
+        }}
       >
-        {/**INICIA LA BARRA LATERAL  h-full py-4 overflow-y-auto p-5*/}
-        <div class="h-full py-4 overflow-y-auto p-5  bg-red-600">
-          <h2 className="text-3xl  font-bold mt-16 mb-8 ">Categorías</h2>
-
-          <ul>
-            {menuItems.map((item, index) => (
-              <motion.li
-                key={index}
-                className={`flex items-center mb-1 ${styleLink}`}
-              
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-              >
-                <Icon icon={item.icon} className="mr-2 size-6" />
-                {item.text}
-              </motion.li>
-            ))}
-          </ul>
-        </div>
-
-        {/***TERMINA LA BARRA LATERAL *** */}
+        <style>
+          {`
+            #sidebar::-webkit-scrollbar {
+              display: none;
+            }
+          `}
+        </style>
+        <h2 className="text-2xl font-bold mt-16 mb-8">Categorías</h2>
+        <ul>
+          {menuItems.map((item, index) => (
+            <motion.li
+              key={index}
+              className={`flex items-center mb-1 ${styleLink}`}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <Icon icon={item.icon} className="mr-2 size-6" />
+              {item.text}
+            </motion.li>
+          ))}
+        </ul>
       </motion.aside>
-      {/*ES LA PARTE DONDE IRIAN LAS TARJETAS */}
-      <div class="p-4 sm:ml-64 bg-orange-200 ">
-        {/**INICIA 1 TARJETA */}
-        <div className="flex flex-wrap">
-          <div className="max-w-sm rounded-xl overflow-hidden shadow-lg m-4 bg-zinc-800 relative">
-            <img
-              className="w-full h-36 object-cover rounded-xl"
-              src="/img/Login.jpg"
-            />
-            <div className="px-6 py-4 flex justify-between items-center">
-              <div className="w-full">
-                <div className="font-bold text-xl mb-2"></div>
-                <p className="text-white text-lg">Camarones para pelar</p>
-                <div className="flex items-center justify-between ">
-                  <p className="text-orange-500 font-bold text-2xl ">$140</p>
-                  <MenuButton />
+
+      <main className="flex-1 p-5 overflow-y-auto ">
+        <h1 className="text-white text-5xl font-semibold mt-14 mb-8 text-center">
+          Menu
+        </h1>
+        <div className="flex mb-6">
+          {" "}
+          <InputSearch></InputSearch>
+          {/* InputSearch component */}
+        </div>
+        {/* AQUI VAN LAS TARJETAS */}
+        <div className="overflow-y-auto h-[64vh]" style={{ scrollbarWidth: "none" }} >
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {menuItems.map((item, index) => (
+              <div
+                key={index}
+                className="max-w-sm rounded-xl overflow-hidden bg-zinc-800 relative"
+              >
+                <img
+                  className="w-full h-36 object-cover rounded-xl"
+                  src="/img/Login.jpg"
+                  alt={item.text}
+                />
+                <div className="px-6 py-4 flex justify-between items-center">
+                  <div className="w-full">
+                    <div className="font-bold text-xl mb-2"></div>
+                    <p className="text-white text-lg">{item.text}</p>
+                    <div className="flex items-center justify-between">
+                      <p className="text-orange-500 font-bold text-2xl">$140</p>
+                      {/* <MenuButton /> */}
+                      <MenuButton/>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
-        {/**TERMINA 1 TARJETA */}
+      </main>
 
-
-        
-      </div>
-      {/**FINAL DEL ESPACIO DE TRAJETAS  */}
+      <button
+        className={`lg:hidden absolute top-5 left-5 z-20 ${
+          sidebarVisible ? "hidden" : ""
+        }`}
+        onClick={toggleSidebar}
+      >
+        <Icon
+          icon="iconamoon:menu-burger-horizontal-bold"
+          className="text-2xl mt-24"
+        />
+      </button>
     </div>
   );
 };
