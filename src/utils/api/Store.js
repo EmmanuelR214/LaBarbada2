1ba5ce26f7e8924eb1cbdb4023f8ba24320c61d8
Emmanuel <emmanuelreyes214@gmail.com>
2024-10-22 15:59:57 -0600

    Add script.sh GHA (chechout) i script 5

--- src/utils/api/Store.js
+++ src/utils/api/Store.js
@@ -6,15 +6,12 @@ export const TraerCarritoRoute = idUser => apiSaurce.get(`/get-shoppingCar/${idU
 
 export const PrecioRoute = precio => apiSaurce.post('/precio-platillo', precio)
 
-<<<<<<< HEAD
 export const DetalleXPrecio = precio => apiSaurce.post('/detalle-x-precio',precio)
-=======
+
 export const InsertarCarritoRoute = carrito => axios.post('/shoppingcar', carrito)
->>>>>>> 6b88c959a0c3ab7f2e6437e130581cac5fa36c9f
 
-export const InsertarCarritoRoute = carrito => apiSaurce.post('shoppingcar', carrito)
+//export const InsertarCarritoRoute = carrito => apiSaurce.post('shoppingcar', carrito)
 
-<<<<<<< HEAD
 export const UpdateShoppingCarRoute = car => apiSaurce.put('/update-shoppingcar', car)
 
 export const DeleteCarRoute = id_car => apiSaurce.delete(`delete-shoppingcar/${id_car}`)
@@ -22,9 +19,8 @@ export const DeleteCarRoute = id_car => apiSaurce.delete(`delete-shoppingcar/${i
 export const CrearVentaRoute = venta => apiSaurce.post('/venta', venta)
 
 export const PagoTarjetaRoute = pago => apiSaurce.post('/pago-tarjeta', pago)
-=======
-export const DeleteCarRoute = id_car => axios.delete(`/delete-shoppingcar/${id_car}`)
->>>>>>> 6b88c959a0c3ab7f2e6437e130581cac5fa36c9f
+
+//export const DeleteCarRoute = id_car => axios.delete(`/delete-shoppingcar/${id_car}`)
 
 
 
--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,14 +1,9 @@
 import axios from "axios"
 
-<<<<<<< HEAD
 export const apiUser = axios.create({
-  baseURL: "http://localhost:3000/api",
-=======
-const instance = axios.create({
   // baseURL:'api.labarbada.store/api',
   //baseURL:'http://localhost:3000/api',
   baseURL:'https://api-barbada.vercel.app/api',
->>>>>>> 6b88c959a0c3ab7f2e6437e130581cac5fa36c9f
   withCredentials: true
 })
 
